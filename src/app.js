4614fa88b0c5967ac7f5f693affc8ab6f2c2ef43
fernando <fernanj2323@gmail.com>
2019-04-25 13:35:50 -0500

    tenemos un webpack con css, pero sin servidor, debemos agregarle nodejs y express a esto y luego react.

---
+++ src/app.js
@@ -0,0 +1,5 @@
+// aqui es donde se carga todo webpack 
+// anteriormente pensabamos que la aplicacion cargaba desde index.html
+// pero en el caso de webpack carga desde app.js 
+require('./styles/main.css');
+alert('works');
